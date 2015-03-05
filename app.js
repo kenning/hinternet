791520b11202b5d9c1b51a7f9431a3f1eb1162ca
kenning <hwerzog@gmail.com>
2015-03-05 12:15:41 -0800

    fix plus4coins urls and changed back to mongolab

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
 
-var db = monk('localhost:27017/testdb');
-//var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
-//var db = monk(connectionString);
+//var db = monk('localhost:27017/testdb');
+var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
+var db = monk(connectionString);
 
 
 var routes = require('./routes/index');
